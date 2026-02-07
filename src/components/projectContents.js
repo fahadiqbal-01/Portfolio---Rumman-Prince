27a0a1a1bf2b756d96101c35f0fc5d36bccd0f8d
fahadiqbal-01 <fahad.iqba.im.47@gmail.com>
2026-02-07 23:41:00 +0600

    x

--- src/components/projectContents.js
+++ src/components/projectContents.js
@@ -1,11 +1,12 @@
 export default function ProjectContents() {
   return (
-    <div className=" pb-50 " >
+    <div className=" pb-50 ">
       <div className=" h-[60vh] relative select-none">
         <img
           src="/images/projectBanner.jpg"
           property="priority"
           className=" w-full mx-auto absolute left-0 -top-170 -z-20 select-none  "
+          priority="priority"
         />
         <h1 className=" font-Bebas text-[94px] text-white flex flex-col absolute left-[50%] top-[50%] translate-[-50%] ">
           Projects
